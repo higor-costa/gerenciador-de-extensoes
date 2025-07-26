397135024e3632481804969715e646d48ccb986e
Higor Costa <higorcosta972@gmail.com>
2025-07-26 17:58:34 -0300

    feat(script.js): define função para acessar dados do data.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
 
 const containerCards = document.querySelector('.cards');
 const extensionsJson = './data.json';
+
+const loadExtensions = async () => {
+  try {
+    const response = await fetch(extensionsJson);
+    const data = await response.json();
+  } catch (error) {
+    console.log('Erro ao carregar extesões: ' + error);
+  }
+};
+
+loadExtensions();
